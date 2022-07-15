8a0b3751af517e3270b4283359be828fce34d901
Parmicciano <85128225+Parmicciano@users.noreply.github.com>
2022-07-15 19:22:27 +0200

    seo

--- pages/vps.vue
+++ pages/vps.vue
@@ -49,6 +49,10 @@
         </div>
         <br /><br />
         <span>
+        VPS is a term that may sound mystical, but in hindsight you will find that it is a simple technology to understand. Virtual Private Server is a technique implemented by web hosting companies to enable any entity to have multiple servers that allow them to manage their web activities more efficiently.
+
+Because these servers are virtual, site management is more efficient and much more reliable in terms of security. The virtual private server works like the classic server, with one detail: it coexists with other virtual private servers on the same computer.
+<br>
           Unfortunately, these instances don't work exactly like the ones you
           could use at AWS or Google Cloud. Indeed, the instances are hosted on
           servers belonging to individuals (homelab). This allows to provide
@@ -58,6 +62,10 @@
           your interface and declare a domain name. By using CNAME in the dns
           records, you will be able to send your traffic directly to your
           instance.<br /><br />
+          VPS is a common hosting model that is part of the Infrastructure as a Service (IaaS) services offered by many ISPs.VPS uses the resources of a physical server to provide users with the same It can be defined as a virtual machine (VM for short) that provides functionality. A VPS therefore represents the same concept as a VDS (Virtual Dedicated Server).
+
+In commercial web hosting, multiple VPS servers are typically hosted on physical high-performance machines, each with its own operating system (OS) and giving users full root access rights. This allows each server administrator to operate independently of other users on the same hardware. Hardware is managed through a hypervisor: the virtual machine administrator. It is an application for creating and managing virtual machines. The hypervisor defines the virtual environment and provides some of the physical resources such as CPU (central processing unit), RAM (random access memory) and disk space for each virtual server. Root permissions allow VPS users to install all applications supported by the operating system of their choice. These can include web server software, email servers and certain e-commerce applications and blog systems.
+<br><br>
           It works the same way for other ports, like port 3306 for example
           (mysql). You will just have to specify the port to your database
           connection tool (remote port).
